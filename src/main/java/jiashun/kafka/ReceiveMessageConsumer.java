b925853f63090276e99d5ee8d55b68c7a8f1e917
jiashun <jiashun@outlook.com>
2017-07-13 02:38:11 +0800

    试用kafka，设置好了消费端和服务端

---
+++ src/main/java/jiashun/kafka/ReceiveMessageConsumer.java
@@ -0,0 +1,42 @@
+package jiashun.kafka;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class ReceiveMessageConsumer {
+	public static final String SERVERS = "kafka-single:9095";
+	public static final String TOPIC = "jiashun-topic";
+	public static void main(String[] args) {
+
+		Properties props = new Properties();
+		// 定义消息消费者的连接服务器地址
+		props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS);
+		// 消息消费者一定要设置反序列化的程序类，与消息生产者完全对应
+		props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+				StringDeserializer.class.getName());
+		props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+				IntegerDeserializer.class.getName());
+		props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-1");
+		// 定义消费者处理对象
+		Consumer<String, Integer> consumer = new KafkaConsumer<String, Integer>(
+				props);
+		consumer.subscribe(Arrays.asList(TOPIC)); // 设置消费者读取的主题名称
+		boolean flag = true; // 消费者需要一直进行数据的读取处理操作
+		while (flag) { // 一直读取消息
+			ConsumerRecords<String, Integer> allRecorders = consumer.poll(200);
+			for (ConsumerRecord<String, Integer> record : allRecorders) {
+				System.out.println(
+						"key = " + record.key() + "、value = " + record.value());
+			}
+		}
+		consumer.close();
+	}
+}
---
+++ src/main/java/jiashun/kafka/SendMessageProducer.java
@@ -0,0 +1,32 @@
+package jiashun.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class SendMessageProducer {
+	public static final String SERVERS = "kafka-single:9095";
+	public static final String TOPIC = "jiashun-topic";
+	public static void main(String[] args) {
+		//如果要进行kafka消息发送需要使用Properties定义环境属性
+		Properties props = new Properties();
+		props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS);//定义kafka服务地址
+		//kafka之中是以key和value的形式进行消息的发送处理，所以为了保证kafka的性能，专门提供有统一类型
+		props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,IntegerSerializer.class.getName());
+		long start = System.currentTimeMillis();
+		//定义消息发送者对象，依靠此对象可以进行消息的传递
+		Producer<String,Integer> producer = new KafkaProducer<String,Integer>(props);
+		for(int x=0;x<10000;x++){
+			producer.send(new ProducerRecord<String, Integer>(TOPIC, "jiashun-"+x,x));
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("消息发送完成："+(end - start));
+		producer.close();
+	}
+}
