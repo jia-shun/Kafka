ddf2083fb2cccb5d4a47e1a8076d9ffdd82fd8a4
jiashun <jiashun@outlook.com>
2017-07-13 23:43:23 +0800

    更新消费端

--- src/main/java/jiashun/kafka/ReceiveMessageConsumer.java
+++ src/main/java/jiashun/kafka/ReceiveMessageConsumer.java
@@ -34,7 +34,7 @@ public class ReceiveMessageConsumer {
 			ConsumerRecords<String, Integer> allRecorders = consumer.poll(200);
 			for (ConsumerRecord<String, Integer> record : allRecorders) {
 				System.out.println(
-						"key = " + record.key() + "、value = " + record.value());
+						"消息接收key = " + record.key() + "、value = " + record.value());
 			}
 		}
 		consumer.close();
